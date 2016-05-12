1848b105d617e12829cb398f7be6cb7bcfdf8817
Tomio Mizoroki <tmizoroki@gmail.com>
2016-05-11 20:27:40 -0700

    Set up the map routing config file

---
+++ src/app/map/config.route.js
@@ -0,0 +1,16 @@
+(function () {
+  'use strict';
+  
+  angular
+    .module('app.map')
+    .config(configFunction);
+  
+  configFunction.$inject = ['$routeProvider'];
+  
+  function configFunction($routeProvider) {
+    $routeProvider.when('/', {
+      templateUrl: 'app/map/map.html'
+    }); 
+  }
+  
+}());
